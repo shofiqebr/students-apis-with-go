35b8f08de3d1150d7c09b877b4a3d14dfccb7a68
Md. shofiqul islam <shofiqebr@gmail.com>
2025-12-01 06:56:24 +0600

    create sutdent create api

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/shofiqebr/students-apis/internal/config"
+	"github.com/shofiqebr/students-apis/internal/http/handlers/student"
 )
 
 func main() {
@@ -20,9 +21,7 @@ func main() {
 	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func (w http.ResponseWriter, r *http.Request)  {
-		w.Write([]byte("welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New() )
 	//  setup server
 
 	server := http.Server{
